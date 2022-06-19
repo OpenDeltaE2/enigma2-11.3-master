a2c867d1d0034639cc57de51ed45f4671693a1e4
OpenDeltaImage <86120617+OpenDeltaImage@users.noreply.github.com>
2022-06-19 04:04:38 +0200

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -36,9 +36,8 @@ class About(Screen):
 		AboutText += _("Image: ") + about.getImageTypeString() + "-" + getImageBuild() + "\n"
 		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"
 		AboutText += _("Last update: ") + about.getUpdateDateString() + "\n"
-	
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
-		AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
+		# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
 
 		EnigmaVersion = about.getEnigmaVersionString()
 		EnigmaVersion = EnigmaVersion.rsplit("-", EnigmaVersion.count("-") - 2)
